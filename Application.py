1d97236b1dcdc31a457842a739cacda0388dacc2
Ahmad Al-harbi <alharbiahmed21@gmail.com>
2022-12-12 17:46:10 +0300

    'nnn'

--- Application.py
+++ Application.py
@@ -1,5 +1,4 @@
 from flask import Flask
-#from views import view 
 
 
 application = Flask(__name__)
@@ -9,7 +8,3 @@ application = Flask(__name__)
 def hello():
     return 'high'
 
-#application.register_blueprint(view, url_prefix="/")
-
-#if __name__ == '__main__':
- #   application.run(debug=True)
