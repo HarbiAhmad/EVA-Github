0a2974cd776bcf23b080e13f40700098dadab0b5
Ahmad Al-harbi <alharbiahmed21@gmail.com>
2022-12-12 17:36:59 +0300

    ‘Smal’

--- Application.py
+++ Application.py
@@ -1,9 +1,15 @@
 from flask import Flask
-from views import view 
+#from views import view 
 
 
 application = Flask(__name__)
-application.register_blueprint(view, url_prefix="/")
 
-if __name__ == '__main__':
-    application.run(debug=True)
+@application.route('/')
+
+def hello():
+    return 'high'
+
+#application.register_blueprint(view, url_prefix="/")
+
+#if __name__ == '__main__':
+ #   application.run(debug=True)
